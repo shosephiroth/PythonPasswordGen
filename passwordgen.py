fc3475d3e5b499008208a20d5a6333b8293d369a
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-04-10 23:45:43 -0400

    Add files via upload
    Will finish this tomorrow.. OT got me

--- passwordgen.py
+++ passwordgen.py
@@ -3,4 +3,4 @@ import array
 
 MAX_LEN = 12
 DIGITS = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
-LOWCASE_CHARS = ['a', 'b', 'c', 'd', ]
+LOWCASE_CHARS = ['a', 'b', 'c', 'd', 'e' 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p, 'q', ']
